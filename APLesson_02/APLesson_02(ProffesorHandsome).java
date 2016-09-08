c95da7991a6a43a335b7246f162b214f01310325
antonschuh <antonschuh1299@gmail.com>
2016-09-08 09:12:22 -0700

    Added Files

---
+++ APLesson_02/APLesson_02(ProffesorHandsome).java
@@ -0,0 +1,10 @@
+
+public class APLesson_02 
+{
+	public static void main(String[]args)
+	{
+		String name = "ProfessorHandsome";
+		System.out.println(name);
+	}
+
+}
--- APLesson_02/APLesson_02.java
+++ APLesson_02/APLesson_02.java
@@ -1,10 +1,14 @@
-
-public class APLesson_02 
+public class APLesson_02
 {
 	public static void main(String[]args)
 	{
-		String name = "ProfessorHandsome";
-		System.out.println(name);
+		int l = 8;
+		int w = 5;
+		int recArea = l * w;
+		System.out.println("The area of your rectangle is" +" "+ recArea);
 	}
-
 }
+	
+	
+		
+	
