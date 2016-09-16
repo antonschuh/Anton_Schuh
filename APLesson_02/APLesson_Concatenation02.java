6a872509ace00dbc05f7dfac88f1d49d9a4abbe7
antonschuh <antonschuh1299@gmail.com>
2016-09-16 08:33:37 -0700

    Added Files

---
+++ APLesson_02/APLesson_Concatenation02.java
@@ -0,0 +1,19 @@
+public class APLesson_Concatenation02
+{
+	public static void main(String[]args)
+	{
+		int num = 5028;
+		String first = "Seachase";
+		String second = "Way";
+		String third = "SanDiego";
+		String fourth = "California";
+		int numSecond = 92130;
+		
+		System.out.println("My address is" +" "+ num +" "+ first +" "+ second +" "+ "and I live in" + " " + third +" "+ fourth +" "+ numSecond);
+		
+		
+			
+		
+		
+	}
+}
