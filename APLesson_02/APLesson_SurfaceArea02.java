b217afd50b24be5014b520a21d00edcd38e90672
antonschuh <antonschuh1299@gmail.com>
2016-09-16 08:41:51 -0700

    Added Files

---
+++ APLesson_02/APLesson_SurfaceArea02.java
@@ -0,0 +1,12 @@
+public class APLesson_SurfaceArea02
+{
+	public static void main(String[]args)
+	{
+		int l = 2;
+		int w = 2;
+		int h = 13;
+		int SArea = l * w * h;
+		
+		System.out.println("The Surface area of your rectangular prism is" +" "+ SArea);
+	}
+}
