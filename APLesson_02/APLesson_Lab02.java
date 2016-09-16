71a5d431ff56575975b98ffc5f70b989e6e4c584
antonschuh <antonschuh1299@gmail.com>
2016-09-16 08:05:56 -0700

    Added Files

---
+++ APLesson_02/APLesson_Lab02.java
@@ -0,0 +1,10 @@
+public class APLesson_Lab02
+{
+	public static void main(String[]args)
+	{
+			int x = 10;
+			int y = 20;
+			int product = x * y;
+			System.out.println("10 and 20 multiplied is" +" "+ product);
+	}
+}
