334682cacbcb901436de7e7ec36895c66fe0e93d
antonschuh <antonschuh1299@gmail.com>
2016-09-20 09:17:54 -0700

    Added Files

---
+++ APLESSON_03/Lab_BMIcalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; //import Staement
+
+public class Lab_BMIcalculator
+{
+	public static void main(String[]args)
+	{
+		//instantiate the new Scanner object "keboard"
+		Scanner keyboard = new Scanner(System.in);
+		
+		//prompt for user imput
+		System.out.println("What is your height?");
+		
+		double height = keyboard.nextDouble();
+		
+		System.out.println("What is your weight?");
+		
+		double weight = keyboard.nextDouble();
+		
+		System.out.println("Your BMI is " + (weight * 703)/(height * height));
+	}
+}
