4a84c61387033ac109eccf3fac40bf7c73fe1cfc
antonschuh <antonschuh1299@gmail.com>
2016-09-26 08:24:59 -0700

    Added Files

--- APLesson_02/APLesson_Concatenation02.java
+++ APLesson_02/APLesson_Concatenation02.java
@@ -3,13 +3,15 @@ public class APLesson_Concatenation02
 	public static void main(String[]args)
 	{
 		int num = 5028;
+		String name = "Anton";
+		String LastName = "Schuh";
 		String first = "Seachase";
 		String second = "Way";
 		String third = "SanDiego";
-		String fourth = "California";
+		String fourth = "CA";
 		int numSecond = 92130;
 		
-		System.out.println("My address is" +" "+ num +" "+ first +" "+ second +" "+ "and I live in" + " " + third +" "+ fourth +" "+ numSecond);
+		System.out.println(name +" "+ LastName + "\n" + num +" "+ first +" "+ second +"\n"+ third +","+" "+ fourth +" "+ numSecond);
 		
 		
 			
