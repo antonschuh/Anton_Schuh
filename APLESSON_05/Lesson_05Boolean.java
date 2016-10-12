875337cddd15bc178f7b9cf50e041117235ca593
antonschuh <antonschuh1299@gmail.com>
2016-10-12 08:26:47 -0700

    Added Files

---
+++ APLESSON_05/Lesson_05Boolean.java
@@ -0,0 +1,17 @@
+public class Lesson_05Boolean
+{
+	public static void main(String[]args)
+	{
+		boolean tOrf = true;
+		
+		if (tOrf())
+		{
+			System.out.println("it is true!");
+		}
+	}
+	
+	public static boolean tOrf()
+	{
+		return true;
+	}
+}
---
+++ APLESSON_05/Lesson_5.java
@@ -0,0 +1,17 @@
+public class Lesson_5
+{
+	public static void main(String[]args)
+	{
+		int num = 3;
+		
+		if(num == 3)
+		{
+			System.out.println("num equals 3!");
+		}
+		
+		if (num == 5)
+		{
+			System.out.println("num equals 5!");
+		}
+	}
+}
