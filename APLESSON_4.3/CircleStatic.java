0ce5331a9bd83a31863e65c2276a1f39fa46d026
antonschuh <antonschuh1299@gmail.com>
2016-10-24 08:20:51 -0700

    Added Files

--- APLESSON_4.3/CircleStatic.java
+++ APLESSON_4.3/CircleStatic.java
@@ -2,27 +2,26 @@ import java.util.Scanner;
 
 public class Circle
 {
-	public double r;
-	public double area;
+	static double r;
+	static double area;
 	
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		Circle round = new Circle();
 		
 		System.out.println("Please enter the radius of your circle: ");
-		round.r = kb.nextInt();
+		r = kb.nextInt();
 		
-		round.area();
-		round.print();
+		area();
+		print();
 	}
 	
-	public double area()
+	public static void area()
 	{
-			return area = 3.14*(r*r);
+			area = 3.14*(r*r);
 	}
 	
-	public void print()
+	public static void print()
 	{
 			System.out.printf("The area of your circle with a radius of" +r+ " %.5f is", area);
 	}	
