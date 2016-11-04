f5fae6b5a7a9314dd44ff0bc98b9be50a9825dd6
antonschuh <antonschuh1299@gmail.com>
2016-11-03 20:36:14 -0700

    Added Files

---
+++ APLESSON_06/GraphTable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class GraphTable 
+{
+	public static void main(String[]args)
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.println(i + "\t|\t" + (2*i + 3));
+		}
+	}
+}
