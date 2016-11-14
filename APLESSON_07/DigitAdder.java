045789f6e0d4636d191480aec9ec8d37e914d8df
antonschuh <antonschuh1299@gmail.com>
2016-11-13 18:44:13 -0800

    Added Files

---
+++ APLESSON_07/DigitAdder.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class DigitAdder 
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		
+		int num = kb.nextInt();
+		
+		System.out.println("The sum of the digits in " + num + " is " + sumDigits(num));
+		
+	}
+	
+	public static int sumDigits(int number)
+	{
+		int sum = 0;
+		int num = number;
+		
+		while (num > 0) 
+		{
+			sum = sum + num%10;
+			num = num/10;
+		}
+		
+		return sum;
+	} 
+}
