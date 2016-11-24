1055269efce08fcbc762c614dfdd9940b023f217
antonschuh <antonschuh1299@gmail.com>
2016-11-23 16:22:50 -0800

    Added Files

---
+++ APLESSON_08/TreeDeg60.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TreeDeg60 
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a word: ");
+		String word = kb.nextLine();
+		
+		tree(word, 0, word.length());
+	}
+	public static void tree(String word, int start, int stop)
+	{
+		if(start <= stop) 
+		{
+			System.out.printf("%10s \n", word.substring(0, start));
+			start ++;
+			tree(word, start, stop);
+		}
+	}
+}
