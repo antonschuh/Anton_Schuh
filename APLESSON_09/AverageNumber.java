15c1aa76e53d93f3565a921c9abc8dab591c2966
antonschuh <antonschuh1299@gmail.com>
2016-12-05 08:14:31 -0800

    Added Files

---
+++ APLESSON_09/AverageNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AverageNumber
+{
+	public static void main(String[]args)
+	{
+		Int [] numbers = new Int[10];
+		Scanner kb = new Scanner(System.in);
+		Syste
+	}
+}
