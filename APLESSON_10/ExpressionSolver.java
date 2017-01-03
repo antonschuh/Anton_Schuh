c484bb584bdb4633b9178c52af072062d890f29a
antonschuh <antonschuh1299@gmail.com>
2017-01-02 17:41:20 -0800

    Added Files

--- APLESSON_10/ExpressionSolver.java
+++ APLESSON_10/ExpressionSolver.java
@@ -1,5 +1,6 @@
-import java.util.ArrayLists;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ExpressionSolver
 {
@@ -9,20 +10,42 @@ public class ExpressionSolver
 		System.out.println("Please enter an equation: ");
 		String expression = kb.nextLine();
 		
-		ArrayList<String>equation = new ArrayList<>(Arrays.asList(expression.split(" ")));
+		ArrayList<String>equation = new ArrayList<>
+									(Arrays.asList(expression.split(" ")));
 		
 		int i = 0;
-		while(i < equation.size())
+		while( i < equation.size() )
 		{
-			if(i < equation.size())
+			if( i < equation.size() && equation.get(i).equals("+") )
 			{
-				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + Integer.parseInt(equation.get(i+1))));
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + 
+								Integer.parseInt(equation.get(i+1))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else if( i < equation.size() && equation.get(i).equals("-") )
+			{
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) - 
+								Integer.parseInt(equation.get(i+1))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else if( i < equation.size() && equation.get(i).equals("*") )
+			{
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) * 
+								Integer.parseInt(equation.get(i+1))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else if( i < equation.size() && equation.get(i).equals("/") )
+			{
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) / 
+								Integer.parseInt(equation.get(i+1))));
 				equation.remove(i-1);
 				equation.remove(i);
 			}
 			else
 				i++;
-		
 		}
 		System.out.println(equation);
 	}
