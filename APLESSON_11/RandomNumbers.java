7eb72b473424eb06f48bcc09eb365163aa480505
antonschuh <antonschuh1299@gmail.com>
2017-01-05 09:12:07 -0800

    AddedFiles

--- APLESSON_11/RandomNumbers.java
+++ APLESSON_11/RandomNumbers.java
@@ -1,13 +1,24 @@
 public class RandomNumbers
 {
 	public static void main(String[]args)
-	{
-		int numbers = 
+	{ 
+		int nums = "1, 2, 3, 4, 5, 6, 7, 8, 9";
 		int[][]randNums = new int [4][4];
-		randNums [0][0] = 1;
-		randNums [1][1] = 2;
-		randNums [2][2] = 3;
-		randNums [3][3] = 4;
+		
+			randNums [0][0] = 1;
+			randNums [1][1] = 2;
+			randNums [2][2] = 3;
+			randNums [3][3] = 4;
+		
+		int num = 1;
+		for(int i = 0; i < randNums.length; i++)
+		{
+			for(int j = 0; j < randNums[i].length; j++)
+			{
+				randNums[i][j] = num;
+			}
+			num++;
+		}
 		
 		for(int i = 0; i < randNums.length; i++)
 		{
@@ -15,7 +26,7 @@ public class RandomNumbers
 			{
 				System.out.print(randNums[i][j] + "\t");
 			}
-			
-		}
+			System.out.println();
+		}	
 	}
 }
---
+++ APLESSON_11/Strings.java
@@ -0,0 +1,16 @@
+Public class Strings
+{
+	public static voi dmain(String[]args)
+	{
+		String[][]words = new Sting [4][4];
+		words [0][0] = ;
+		words [1][1] = ;
+		words [2][2] = ;
+		words [3][3] = ;
+		
+		for(int i = 0; i < words.length; i++)
+		{
+			System.out.println("Please enter a word: ");
+		}
+	}
+}
