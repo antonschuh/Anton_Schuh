1218c5da1d0be69e00f257ea05845bcb4b0f0850
antonschuh <antonschuh1299@gmail.com>
2017-01-09 08:08:16 -0800

    Added Files

---
+++ APLESSON_11/Divisor.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Divisor
+{
+	publis static void main(String[]args)
+	{
+		int[][] nums = new int [4][4];
+		
+		for(int i = 0; i < nums.length; i++)
+		{
+			for(int j = 0; j < nums[i].length; i++)
+			{
+				num[i][j] = rand.nextInt(100 - 1 + 1) + 1;
+			}
+		}
+		
+		for(int i = 0; i < nums.length =; i++)
+		{
+			for(int j = 0; j < nums[i].length; i++)
+			{
+				System.out.print(nums[i][j] + "\t");
+			}
+			System.out.println();
+		}			
+	}
+}
