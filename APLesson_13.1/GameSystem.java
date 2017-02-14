f4e1fcc293bfec6ac143d5aec5a16628002024ed
antonschuh <antonschuh1299@gmail.com>
2017-02-14 09:16:33 -0800

    Added Files

--- APLesson_13.1/GameSystem.java
+++ APLesson_13.1/GameSystem.java
@@ -1,1 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class GameSystem
+{
+	ArrayList<Game> gamelist = new ArrayList<Game>();
+	
+	public GameSystem()
+	{
+		super();
+	}
+	
+	public GameSystem(String List)
+	{
+		loadGames(list);
+	}
+	 
+	public void loadGames(String list)
+	{
+		ArrayList<String>Games = new ArrayList<>
+		(Arrays.asList(list.split(", ")));
+		
+		String platform = "";
+		Int serialNo = "";
+		
+		for(int i = 0; i < game.size(); i = i+2)
+		{
+			if( getThatGame(games.get(i)) == null )
+				if(gmaes.get(i+1).equals("console
+			gameList.add( new Console games.get)
+		}
+	}
+}
